8a99049ed3580b12eff9a00bc8d257222ca5b15c
Lucas Mançan <lucasfmancan@hotmail.com>
2022-11-19 20:05:55 -0300

    Update pmpro_redirect_based_on_user_role.php

--- pmpro_redirect_based_on_user_role.php
+++ pmpro_redirect_based_on_user_role.php
@@ -8,23 +8,24 @@
  */
 function my_pmpro_redirect_non_members() {
 
- 
  	global $pmpro_pages;
 
-	if( is_page( $pmpro_pages ) || is_home() || current_user_can( 'manage_options' )) {
+	if( is_page( $pmpro_pages ) || is_front_page() || current_user_can( 'manage_options' )) {
 		return;
 	}
 
 	$access = false;
+		
+	global $current_user, $pmpro_pages;
 	
 	$current_user = wp_get_current_user();
-
 	$user_id = $current_user->ID;
-
+	$post_id = $post->ID;
+	
 	if( !empty( $user_id ) ) {
 		// Get approval status
-		if(pmpro_has_membership_access( $post->ID ) ) {
-			$access = true;
+		if(pmpro_has_membership_access( $post_id ) ) {		
+			 $access = true;
 		} else {
 			$access = false;
 		}
@@ -33,12 +34,23 @@ function my_pmpro_redirect_non_members() {
 		if( ! pmpro_hasMembershipLevel() ) {
 			$access = false;
 		}
+		
+		
+		// if the user is not approved, redirect to BuddyPress restricted page or home page if add on not enabled.
+		if ( ! $access ) {
+			wp_redirect( site_url('/blog/') );
+			exit;
+		}
+	} else {
+		if(pmpro_has_membership_access( $post_id ) ) {		
+			$access = true;
+		} else {
+			wp_redirect( site_url('/login/') );
+			exit;
+		}
 	}
+	
+	
 
-	// if the user is not approved, redirect to BuddyPress restricted page or home page if add on not enabled.
-	if ( ! $access ) {
-		wp_redirect( home_url() );
-		exit;
-	}
 }
 add_action( 'template_redirect', 'my_pmpro_redirect_non_members', 45 );
